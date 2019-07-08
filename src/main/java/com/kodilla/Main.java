5846f82eecfc217b6b7c83d66d0ebb564efa696b
TheRitual <dev@theritual.eu>
2019-07-09 01:51:46 +0200

    Covered errors when taking arguments in main()

--- src/main/java/com/kodilla/Main.java
+++ src/main/java/com/kodilla/Main.java
@@ -2,15 +2,15 @@ package com.kodilla;
 
 public class Main {
     public static void main(String[] args) {
-        args = new String[3];
-        args[0] = "123";
-        args[1] = "day";
-        args[2] = "mo";
-        UnitConverter converter = new UnitConverter();
-        converter.loadStandardUnits();
-
-
-
-        System.out.println(args[0] + " " + args[1] + " converted into " + args[2] + " is " + converter.convert(args[0],args[1],args[2]));
+        if(args.length >= 3) {
+            UnitConverter converter = new UnitConverter();
+            converter.loadStandardUnits();
+            System.out.println(args[0] + " " + args[1] + " converted into " + args[2] + " is " + converter.convert(args[0],args[1],args[2]));
+        } else {
+            System.out.println("Invalid parameters. USAGE :::" +
+                    "\n\tunitconverter [value] [unitA] [unitB]\"" +
+                    "\n\tConverts [value] from [unitA] to [unitB]" +
+                    "\n\t\texample: \"converter 100 kg lb\"");
+        }
     }
 }
